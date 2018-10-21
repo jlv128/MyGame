2bdf2dbe66298c5526c0340617eb04995994f4de
jlv128 <jinlv82@gmail.com>
2018-10-21 22:53:36 +1000

    first change

---
+++ src/csse2002/block/world/WorldMapInconsistentException.java
@@ -0,0 +1,18 @@
+package csse2002.block.world;
+
+/**
+ * A World Map file is geometrically inconsistent.
+ */
+public class WorldMapInconsistentException
+        extends csse2002.block.world.BlockWorldException {
+    /**
+     * WorldMapInconsistentException
+     */
+    public WorldMapInconsistentException() {}
+
+    /**
+     * WorldMapInconsistentException
+     * @param message a message.
+     */
+    public WorldMapInconsistentException(java.lang.String message) {}
+}
---
+++ test/csse2002/block/world/SparseTileArrayTest.java
@@ -0,0 +1,17 @@
+package csse2002.block.world;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SparseTileArrayTest {
+
+  @Test
+  public void getTile() {}
+
+  @Test
+  public void getTiles() {}
+
+  @Test
+  public void addLinkedTiles() {}
+}
